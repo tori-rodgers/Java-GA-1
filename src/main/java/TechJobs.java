51c1e718a9bba89255fb7d6c16d4725f9f2b8a32
Tori Rodgers <torirodgers22@gmail.com>
2024-10-20 21:02:12 -0500

    task one complete

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,16 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+        } else {
+            for(HashMap<String, String> jobs: someJobs) {
+                System.out.println("\n*****");
+                for (String keys : jobs.keySet()) {
+                    System.out.println(keys + ": " + jobs.get(keys));
+                }
+                System.out.println("*****");
+            }
+        }
     }
 }
